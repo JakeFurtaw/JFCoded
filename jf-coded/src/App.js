fc9194ffa21b043ae41d8676b75c075474ceddd3
JakeFurtaw <jfurtaw1@students.towson.edu>
2023-12-13 08:58:27 -0500

    added homepage

--- jf-coded/src/App.js
+++ jf-coded/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Route} from 'react-router-dom';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
-    <div className='landing-page'> 
-    <h1> Welcome to JFCoded! </h1>
+    <div>
+      <BrowserRouter>
+        <Route exact path="/" element={<Homepage/>} />
+      </BrowserRouter>
     </div>
+
   );
 }
 
---
+++ jf-coded/src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+const Homepage = () => {
+    return (
+        <Fragment>
+            <div>
+                <h1>Welcome to JFCoded!</h1>
+                <p>This website serves as a resume and project place for future employeers 
+                    and investors to see what I bring to the table.</p>
+
+            </div>
+            
+        </Fragment>
+    );
+};
+
+export default Homepage;
