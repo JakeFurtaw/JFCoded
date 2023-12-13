d8dfc7f70db19b3d3fa4f9de9fbd448bf6a3f3a9
JakeFurtaw <jfurtaw1@students.towson.edu>
2023-12-13 15:37:52 -0500

    Updated favicon, added homepage functionality

--- jf-coded/src/App.js
+++ jf-coded/src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
+
 import React from 'react';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
-
-
 
 function App() {
   return (
--- jf-coded/src/pages/Homepage.jsx
+++ jf-coded/src/pages/Homepage.jsx
@@ -1,19 +1,51 @@
-import {Fragment, React, useState} from 'react';
+import {Fragment, React, useState, useEffect} from 'react';
 import '../stylesheets/Homepage.css';
 import JFCoded from '../images/JFCoded.jpg';
+import EmployerImg from '../images/employerImg.jpg';
+import InvestorImg from '../images/investor.jpg';
+import CuriousImg from '../images/curious.jpg';
+
+// **---------------------------------------------------------**
+// **----------------------Homepage Code----------------------**
+// **---------------------------------------------------------**
 const Homepage = () => {
+
+
+    const [typedText, setTypedText] = useState('');
+    const fullText = 'This website sereves as a Resume and Project Repository for future employers and investors to checkout and see what I bring to the table..';
+
+    useEffect(() => {
+    let i = 0;
+    const typeWriterInterval = setInterval(() => {
+    setTypedText(fullText.substring(0, i++));
+    if (i === fullText.length) {
+    clearInterval(typeWriterInterval);
+    }
+    }, 30); // Adjust delay for typing speed
+    }, []);
+
     return (
         <Fragment>
             <div className='homepage-container'>
-                <h1>Welcome to JFCoded</h1>
+                <h1>Welcome to JFCoded!</h1>
                 <img className='jfcoded-img' style={{width: 300, height: 300}} src={JFCoded} alt="Loading SVG" />
-                <p>This website serves as a resume and project depot for any future employers and investors. Click the 
-                    button that applies to you to get started.</p>
-                <div >
-                    <h1>Are you an...</h1>
-                    <a href='./Employer.jsx' className='link-options'>Employeer</a>
-                    <a className='link-options'>Investor</a>
-                    <a className='link-options'>Just Curious</a>
+                <p style={{padding: 30, fontSize: 'large', textEmphasis: 'bold'}}>{typedText}</p>
+                <div className='homepage-selector-section'>
+                <h1 className='link-title'>Are you an...</h1>
+                    <div className='homepage-link-section'>
+                        <div className='link-cell'>
+                            <a href='./Employer.jsx' className='link-options'>Employer</a>
+                            <img style={{width: 204, height: 306}} src={EmployerImg} alt="" />
+                        </div>
+                        <div className='link-cell'>
+                        <a className='link-options'>Investor</a>
+                            <img style={{width: 204, height: 306}} src={InvestorImg} alt="" />
+                        </div>
+                        <div className='link-cell'>
+                        <a className='link-options'>Just Curious</a>
+                            <img style={{width: 204, height: 306}} src={CuriousImg} alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </Fragment>
