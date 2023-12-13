242dddd86e523eff78fe8061a07dfbe86d84f20a
JakeFurtaw <jfurtaw1@students.towson.edu>
2023-12-13 18:12:37 -0500

    a

--- jf-coded/src/App.js
+++ jf-coded/src/App.js
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Homepage />} />
-          <Route path='/Employer' element={<Employer/>} />
-          <Route path='/*' element={<h1>Not Found</h1>} />
+          <Route path='/Employer.jsx' name='Employer' element={<Employer/>} />
+          <Route path='/*' element={<h1>404 Error. Page not found....</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
