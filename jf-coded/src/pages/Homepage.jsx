36aac1a4d7a32946172bac8ec4c614bfd5ab64cd
JakeFurtaw <jfurtaw1@students.towson.edu>
2023-12-13 09:25:06 -0500

    first commit with new project

---
+++ jf-coded/src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Homepage = () => {
+    return (
+        <div className='homepage-container'>
+            <h1>Welcome to JFCoded</h1>
+            <p>This website serves as a resume and project depot for any employers or investors.</p>
+            <div>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Homepage;
