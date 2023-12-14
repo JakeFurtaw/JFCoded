3b12bd5b676678a3e59bf03948cd0fdda8445284
JakeFurtaw <jfurtaw1@students.towson.edu>
2023-12-13 21:26:18 -0500

    a

--- jf-coded/src/components/Education.jsx
+++ jf-coded/src/components/Education.jsx
@@ -7,13 +7,13 @@ const Education = () => {
             <h3>Education</h3>
             <div className='education-sections'>
                 <div id='tu' className='education'>
-                    <h4>Towson University</h4>
+                    <h3>Towson University</h3>
                     <p>Graduated: December 2023</p>
                     <p>Bachelors of Science in Computer Science <br/> Focus on Software Engineering</p>
                 </div>
 
                 <div id='hcc' className='education'>
-                    <h4>Howard Community College</h4>
+                    <h3>Howard Community College</h3>
                     <p>Graduated: December 2021</p>
                     <p>Assocaites of Science in Computer Science</p>
                 </div>
