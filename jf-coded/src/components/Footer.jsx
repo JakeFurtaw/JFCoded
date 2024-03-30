eef869d0985283f3b152cc9f997e46ee3d6d8ca3
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-30 13:22:14 -0400

    updated footer with new links

--- jf-coded/src/components/Footer.jsx
+++ jf-coded/src/components/Footer.jsx
@@ -16,6 +16,12 @@ const Footer = () => {
                     <li>
                         <Link to="https://github.com/JakeFurtaw">My Github</Link>
                     </li>
+                    <li>
+                        <Link to="https://gitlab.com/JakeFurtaw">My Gitlab</Link>
+                    </li>
+                    <li>
+                        <Link to="https://www.linkedin.com/in/jacob-furtaw-5b5b4b1b2/">My LinkedIn</Link>
+                    </li>
                 </ul>
                 <p>Coded By: Jacob Furtaw & Copilot
                     <br />
