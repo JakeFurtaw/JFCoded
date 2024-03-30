f74aa2cbcb8ef8c30e48803600ef8f492878bd13
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-30 15:40:42 -0400

    a

--- jf-coded/src/pages/Homepage.jsx
+++ jf-coded/src/pages/Homepage.jsx
@@ -34,7 +34,6 @@ const Homepage = () => {
 
     return (
         <Fragment className='homepage-container'>
-            <Analytics />
             <div className='homepage-container'>
                 <h1>Welcome to JFCoded!</h1>
                 <img className='jfcoded-img' style={{width: 300, height: 300}} src={JFCoded} alt="Loading SVG" />
@@ -58,7 +57,9 @@ const Homepage = () => {
                 </div>
             </div>
             <Footer />
+            <Analytics />
         </Fragment>
+        
     );
 };
 
