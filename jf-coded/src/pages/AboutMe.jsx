9ddcff10537bd0e88951e7275f8b71ccb8189b1e
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-30 12:24:50 -0400

    added about me page

--- jf-coded/src/pages/AboutMe.jsx
+++ jf-coded/src/pages/AboutMe.jsx
@@ -1,23 +1,45 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import Footer from '../components/Footer';
-// import '../stylesheets/AboutMe.css';
+import '../stylesheets/Employer.css';
+import Education from '../components/Education';
+import Projects from '../components/Projects';
+import {useState, useEffect} from 'react';
+
 
 
 
 function AboutMe() {
+
+    const [typedText, setTypedText] = useState('');
+    const fullText = 'Hello, my name\'s Jacob Furtaw, I recently graduated from Towson University earning myself a Bachelors of Science in Computer Science with a Focus on Software Engineering. I am currently looking for a job in the field of Software Engineering. My main intrests are Machine Learning and Web Development. I have an unmatched work ethic, insane ability to learn new skills quickly, and I love to code. Get me on the phone and lets talk about how I can help your company today!!';
+
     useEffect(() => {
-        window.scrollTo(0, 0)
-      }, [])
+    let i = 0;
+    const typeWriterInterval = setInterval(() => {
+    setTypedText(fullText.substring(0, i++));
+    if (i === fullText.length) {
+    clearInterval(typeWriterInterval);
+    }
+    }, 20); // Adjust delay for typing speed
+    }, []);
 
     return (
         <Fragment>
-        <div>
-            <h1>About Me</h1>
-            <p>This page is under construction, coming soon...</p>
-        </div>
-        <Footer />
-        </Fragment>
+            <div>
+                <div className='intro'>
+                    <h2 style={{textDecoration: 'none' }}>About Me</h2>
+                    <p style={{padding: 30, fontSize: 'large', textEmphasis: 'bold'}}>{typedText}</p>
+                </div>
+                <div className='resume'>
+                    <h2>My Resume</h2>
+                    <Education/>
+                    <Projects/>
+                    
+                </div>
 
+                <Footer />
+            </div>
+        </Fragment>
     );
 }
 
