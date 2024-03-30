91e09dc7d0e4f4dc0809e07a87985375427ad5df
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-30 15:14:51 -0400

    swapped around edu section

--- jf-coded/src/components/Education.jsx
+++ jf-coded/src/components/Education.jsx
@@ -8,18 +8,17 @@ const Education = () => {
         <div className='education-container'>
             <h3>Education</h3>
             <div className='education-sections'>
-                <div id='tu' className='education'>
-                    <h3>Towson University</h3>
-                    <img style={{width: 70, height: 90}} src={TowsonIMG} alt="" />
-                    <p>Graduated: December 2023</p>
-                    <p>Bachelors of Science in Computer Science</p>
-                </div>
-
                 <div id='hcc' className='education'>
                     <h3>Howard Community College</h3>
                     <img style={{width: 70, height: 90}} src={HCCIMG} alt="" />
                     <p>Graduated: December 2021</p>
                     <p>Assocaites of Science in Computer Science</p>
+                </div>
+                <div id='tu' className='education'>
+                    <h3>Towson University</h3>
+                    <img style={{width: 70, height: 90}} src={TowsonIMG} alt="" />
+                    <p>Graduated: December 2023</p>
+                    <p>Bachelors of Science in Computer Science</p>
                 </div>
             </div>
 
