1196121c7e7d3b0429a65baddb6d5e010677496f
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-30 11:44:36 -0400

    a

--- jf-coded/src/components/Projects.jsx
+++ jf-coded/src/components/Projects.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 const Projects = () => {
     return (
         <div className='project-container'>
-            <h3>Projects</h3>
+            
             <div className='projects'>
-
+            <h2>Projects</h2>
                 <div className='project'>
                     <h3>Using an ASTNN to Equivalently Sort Mutants</h3>
                     <Link style={{color:'#7da7f5'}} to='https://gitlab.com/JakeFurtaw/ASTNN-COSC490'>Gitlab Link</Link>
-                    <h5>Project Description</h5>
-                    <p>Description: For my Software Project Practicum class at Towson we were given an outdated Abstract Syntax Tree Neural Network that was built to automatically identify equivalent mutants.
+                    <h4>Project Description</h4>
+                    <p><strong>Description:</strong> For my Software Project Practicum class at Towson we were given an outdated Abstract Syntax Tree Neural Network that was built to automatically identify equivalent mutants.
                         We were tasked with updating the ASTNN to run on python 3.10, implimenting Code2Vec, adding a learning rate hyperparameter, and getting the model to produce decent results.
                     </p>
                 </div>
@@ -21,7 +21,7 @@ const Projects = () => {
                 <div className='project'>
                     <h3>Using Machine Learning to Automatically Sort Equivalent Mutants</h3>
                     <Link style={{color:'#7da7f5'}} to='https://gitlab.com/JakeFurtaw/ASTNN-COSC490'>Gitlab Link</Link>
-                    <h5>Project Description</h5>
+                    <h4>Project Description</h4>
                     <p>
                     <strong>Description:</strong> For the Software Project Practicum class at Towson University, our team was assigned to work on an outdated Abstract Syntax Tree Neural Network (ASTNN) project. The ASTNN was originally designed to automatically identify equivalent mutants, which are slight variations of a program that produce the same output as the original program.
                     </p>
@@ -41,7 +41,7 @@ const Projects = () => {
                     <h3>Oceans</h3>
                     <Link style={{color:'#7da7f5'}} to='https://oceans-prod-v1.vercel.app/'>Link to Oceans</Link>
                     <h5>Project Description</h5>
-                    <p>Description: Oceans was built in using the MERN stack. Over the course of a semester we built and deployed a full stack web application.
+                    <p><strong>Description:</strong> Oceans was built in using the MERN stack. Over the course of a semester we built and deployed a full stack web application.
                         Oceans is a social media platform that allows users to limit their exposure to information to whatever they want to see, not what they're forced to see. Users can create an account, post content, and interact with other users.
                     </p>
                 </div>
