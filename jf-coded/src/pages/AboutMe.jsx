954d1250a50529d90392a3ce5a5b4b22e265d7bb
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-30 12:29:18 -0400

    a

--- jf-coded/src/pages/AboutMe.jsx
+++ jf-coded/src/pages/AboutMe.jsx
@@ -11,7 +11,7 @@ import {useState, useEffect} from 'react';
 function AboutMe() {
 
     const [typedText, setTypedText] = useState('');
-    const fullText = 'Hello, my name\'s Jacob Furtaw, I am a Towson University gradute. I earned myself a Bachelors of Science in Computer Science with a Focus on Software Engineering. I am currently looking for a job in the field of Software Engineering. My main intrests are Machine Learning and Web Development. I have an unmatched work ethic, insane ability to learn new skills quickly, and I love to code. Get me on the phone and lets talk about how I can help your company today!!';
+    const fullText = 'Hello, my name\'s Jacob Furtaw, I am a Towson University graduate. I earned myself a Bachelors of Science in Computer Science with a Focus on Software Engineering. I am currently looking for a job in the field of Software Engineering. My main intrests are Machine Learning and Web Development. I have an unmatched work ethic, insane ability to learn new skills quickly, and I love to code. Get me on the phone and lets talk about how I can help your company today!!';
 
     useEffect(() => {
     let i = 0;
