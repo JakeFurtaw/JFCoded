5a8b175d2cbbc8e5f51255b42b2d6ce319f6985d
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-03-30 23:01:05 -0400

    updated linked in link

--- jf-coded/src/components/Footer.jsx
+++ jf-coded/src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
                         <Link to="https://gitlab.com/JakeFurtaw">My Gitlab</Link>
                     </li>
                     <li>
-                        <Link to="https://www.linkedin.com/in/jacob-furtaw-5b5b4b1b2/">My LinkedIn</Link>
+                        <Link to="https://www.linkedin.com/in/jacob-furtaw/">My LinkedIn</Link>
                     </li>
                 </ul>
                 <p>Coded By: Jacob Furtaw
