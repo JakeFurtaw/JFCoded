e42109fd968a9e3159e64b2c4a20252102769641
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-09-04 14:08:26 -0400

    Edited projects page to add my stable diffusion project and imgs for that project

--- jf-coded/src/pages/Projects.jsx
+++ jf-coded/src/pages/Projects.jsx
@@ -13,6 +13,11 @@ import JFCodedImg1 from '../images/projectImages/JFCoded/Homepage.png';
 import JFCodedImg2 from '../images/projectImages/JFCoded/Resume-Page.png';
 import JFCodedImg3 from '../images/projectImages/JFCoded/Contact-Page.png';
 
+import ImgAlt1 from '../images/projectImages/ImageAlter/img_alt_ss.png'
+import ImgAlt2 from '../images/projectImages/ImageAlter/advanced_options.png'
+import ImgAlt3 from '../images/projectImages/ImageAlter/gen_imgs.png'
+import ImgAlt4 from '../images/projectImages/ImageAlter/out_img_gal.png'
+import ImgAlt5 from '../images/projectImages/ImageAlter/i2i_ss.png'
 
 import ChatRAGImg1 from '../images/projectImages/Chat-RAG/start_state.png';
 import ChatRAGImg2 from '../images/projectImages/Chat-RAG/model_dropdown.png';
@@ -36,7 +41,7 @@ const Project = ({ name, description, images, githubLink }) => {
         const scrollGallery = () => {
             const scrollMax = gallery.scrollWidth - gallery.clientWidth;
             gallery.scrollLeft = scrollAmount;
-            scrollAmount += 1.5; // Adjust for speed
+            scrollAmount += 1.8; // Adjust for speed
             if (scrollAmount >= scrollMax) {
                 scrollAmount = 0;
             }
@@ -74,7 +79,7 @@ const Projects = () => {
     const highlightedProjects = [
         {
             name: 'Chat RAG',
-            description: 'Chat RAG is an advanced interactive coding assistant that leverages Retrieval-Augmented Generation (RAG) to provide informed responses to coding queries. Built with a user-friendly Gradio interface, it allows users to interact with various language models, customize model parameters, and upload context files for more accurate assistance.',
+            description: 'Chat RAG is an advanced interactive coding assistant that leverages Retrieval-Augmented Generation (RAG) to provide informed responses to coding queries. Built with a user-friendly Gradio interface, it allows users to interact with various language models, customize model parameters, and upload context files from local directories or GitHub repositories for more accurate assistance.',
             images: [ChatRAGImg1, ChatRAGImg2, ChatRAGImg3, ChatRAGImg4],
             githubLink: 'https://github.com/JakeFurtaw/Chat-RAG'
         },
@@ -83,6 +88,12 @@ const Projects = () => {
 
     const mlProjects = [
         {
+            name: 'Image Alter',
+            description: 'Image Alter is a cutting-edge Gradio-based web application that empowers users to create and edit images using advanced Stable Diffusion models. Leveraging state-of-the-art AI technology, including the Flux model by Black Forest Labs and Stable Diffusion XL Refiner 1.0 by Stability AI, the app offers two primary functionalities: Text-to-Image generation and Image-to-Image transformation. With its intuitive interface, Image Alter allows users to transform text prompts into stunning visuals and enhance existing images with AI-driven alterations. Whether you\'re a professional designer, a digital artist, or an enthusiast exploring AI-generated imagery, Image Alter provides a powerful platform to bring your creative visions to life, pushing the boundaries of digital image manipulation.',
+            images: [ImgAlt1, ImgAlt2, ImgAlt3, ImgAlt4, ImgAlt5],
+            githubLink: 'https://github.com/JakeFurtaw/ImageAlter'
+        },
+        {
             name: 'Health Bot',
             description: 'Health Bot is an intelligent health chatbot that provides personalized health-related information through natural language interactions. It uses advanced language models to deliver responsive and context-aware answers, while a user-friendly interface allows easy communication. The system manages chat histories efficiently, ensuring continuity in conversations and preserving interactions for future reference.',
             images: [HealthGImg1, HealthGImg2, HealthGImg3],
@@ -90,7 +101,7 @@ const Projects = () => {
         },
         {
             name: 'Chat RAG',
-            description: 'Chat RAG is an advanced interactive coding assistant that leverages Retrieval-Augmented Generation (RAG) to provide informed responses to coding queries. Built with a user-friendly Gradio interface, it allows users to interact with various language models, customize model parameters, and upload context files for more accurate assistance.',
+            description: 'Chat RAG is an advanced interactive coding assistant that leverages Retrieval-Augmented Generation (RAG) to provide informed responses to coding queries. Built with a user-friendly Gradio interface, it allows users to interact with various language models, customize model parameters, and upload context files from local directories or GitHub repositories for more accurate assistance.',
             images: [ChatRAGImg1, ChatRAGImg2, ChatRAGImg3, ChatRAGImg4],
             githubLink: 'https://github.com/JakeFurtaw/Chat-RAG'
         }
@@ -134,7 +145,7 @@ const Projects = () => {
                     <Project key={index} {...project} />
                 ))}
             </div>
-            <h4>Last Updated 8/30/24 check my GitHub for my latest project updates. More projects coming soon!</h4>
+            <h4>Last Updated 9/4/24 check my GitHub for my latest project updates. More projects coming soon!</h4>
             <h4>Link Below!</h4>
             <SocialLinks />
         </div>
