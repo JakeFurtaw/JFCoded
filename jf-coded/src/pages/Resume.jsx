9bfebea313466ca5cee884cfe37de84974be59dc
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-10-06 13:58:11 -0400

    updated virutal and downloadable resume

--- jf-coded/src/pages/Resume.jsx
+++ jf-coded/src/pages/Resume.jsx
@@ -63,21 +63,17 @@ const Resume = () => {
                     <h2>Intro</h2>
                     <p>Hello my name is Jake and I am an innovative Machine Learning Engineer/Researcher specializing in Natural Language Processing (NLP), seeking
                         opportunities to advance AI technology. I excel in designing, developing, and testing cutting-edge AI Assistants and implementing advanced 
-                        NLP techniques like Retrieval-Augmented Generation(RAG) and agentic RAG to deliver versatile, high-impact solutions. 
+                        NLP techniques like Retrieval-Augmented Generation(RAG) and Agentic RAG to deliver versatile, high-impact solutions. 
                         I excel in both independent and collaborative settings, driving projects from ideation to deployment.
                         
-                        <br/><br/>My expertise spans data preprocessing, synthetic data creation, model training and finetuning,
-                        custom AI assistant development, RAG pipeline creation, and deploying scalable Machine Learning solutions. I thrive in cross-functional 
-                        teams, fostering collaboration to deliver exceptional results. I am passionate about solving complex problems and
-                        pushing AI boundaries. 
+                        <br/><br/>My expertise spans data preprocessing, synthetic data creation, model training and finetuning, custom AI assistant development, RAG pipeline creation, and deploying scalable Machine Learning solutions. I thrive in cross-functional 
+                        teams, fostering collaboration to deliver exceptional results. I am passionate about solving complex problems and pushing AI boundaries. 
                         
-                        <br/><br/> Proficient in Python, PyTorch, and TensorFlow, I am adept at using a large scope of Machine Learning and Data Sceince 
-                        libraries such as HuggingFace, Langchain, Llama-Index, Transformers, and Diffusers to name a few. My technical skills extend to big 
-                        data technologies, version control, and containerization, enabling me to develop robust, efficient AI systems. 
+                        <br/><br/> Proficient in Python I am adept at using a large scope of Machine Learning and Data Sceince libraries such as PyTorch, Tensorflow, Langchain, Llama-Index, Transformers, and Diffusers to name a few. 
+                        My technical skills extend to big data manipulation, version control, cloud implementation, and containerization, enabling me to develop robust, efficient AI systems. 
                         
-                        <br/><br/>While my primary expertise lies in Natural Language Processing and Computer Vision, I am eager to explore new machine 
-                        learning frontiers. Driven by the potential of AI to transform industries and improve lives, I am committed to contributing to 
-                        groundbreaking research and developing innovative solutions that redefine the possibilities in artificial intelligence.
+                        <br/><br/>While my primary expertise lies in Natural Language Processing, I have started to explore Computer Vision and I am eager to explore other machine learning frontiers. Driven by the potential of AI to 
+                        transform industries and improve lives, I am committed to contributing to groundbreaking research and developing innovative solutions that redefine the possibilities in artificial intelligence.
                         
                         <br/><br/><strong style={{fontSize:"1.8rem"}}>Get me on the phone today and lets talk about how I can help your organization thrive!</strong></p>
                 </AnimatedSection>
@@ -94,19 +90,18 @@ const Resume = () => {
                     </div>
 
                     <h3><strong>Machine Learning/Data Science</strong></h3> 
-                    <p>Libraries/Dependencies: PyTorch, TensorFlow, Scikit-Learn, Transformers, Diffusers, PIL, HuggingFace, Langchain, Llama-Index, Llama Parse, Gradio, Pandas, NumPy, Ollama</p>
+                    <p>Libraries/Dependencies: PyTorch, TensorFlow, Scikit-Learn, Transformers, Diffusers, PIL, HuggingFace, Langchain, Llama-Index, Llama Parse, Gradio, Pandas, NumPy, Matplotlib, Ollama</p>
                     <p>Data Stores: Milvus, ChromaDB, Neo4j</p>
-                    <p>Skills: Retrieval-Augmented Generation, Agentic RAG, Prompt Engineering, Data Preprocessing, Synthetic Data Creation, Feature Engineering</p>
+                    <p>Skills: Retrieval-Augmented Generation, Agentic RAG, Prompt Engineering, Data Preprocessing, Synthetic Data Creation, Model Training and Finetuning</p>
                     
                     <h3><strong>Tools & Platforms</strong></h3>
                     <div className="skills-container">
                         <SkillBar name="Git" level={99} />
                         <SkillBar name="PyCharm" level={95} />
-                        <SkillBar name="VS Code" level={70} />
-                        <SkillBar name="Conda" level={90} />
                         <SkillBar name="Jupyter" level={35} />
                         <SkillBar name="Docker" level={25} />
-                        <SkillBar name="NVIDIAs AI Workbench" level={25} />
+                        <SkillBar name="VS Code" level={70} />
+                        <SkillBar name="Conda" level={90} />
                     </div>
             
                     <h3><strong>Operating Systems</strong></h3>
@@ -124,20 +119,20 @@ const Resume = () => {
                         <h3>Machine Learning Research Engineer</h3>
                         <p>SurgePoint Software | Aug 2023 - Present</p>
                         <ul>
-                            <li>Developed an innovative AI Assistant using open-source LLMs like Codestral 22B, Mistral 7B, Mistral-NeMo 12B, and Llama 3.1 8B</li>
-                            <li>Utilized self-taught data engineering and data preprocessing skills to craft a 13-million-line dataset of web-scraped data</li>
-                            <li>Implemented Retrieval-Augmented Generation to increase semantic relevance scores and combat model hallucinations</li>
-                            <li>Leveraged self-taught prompt engineering skills to ensure the model has a well-structured prompt and enough context to answer any query a user might input</li>
-                            <li>Worked with a small, diverse, and time constrained group of individuals at a startup in a remote setting</li>
+                            <li>Developing an innovative AI Assistant using open-source LLMs like Qwen 2.5 7B, Mistral 7B, Mistral-NeMo 12B, and Llama 3.1 8B</li>
+                            <li>Utilizing self-taught data engineering and data preprocessing skills to craft a 13-million-line structured dataset from 200 million lines of unstructured web-scraped HTML and PDF data</li>
+                            <li>Implementing a sophisticated Retrieval-Augmented Generation pipeline to provide the most up-to-date university information to the model to increase semantic relevance scores of retrieved context and reduce model hallucinations</li>
+                            <li>Leveraging self-taught prompt engineering skills to ensure each tested model has a custom well-structured prompt and enough context to answer any query a user might input</li>
+                            <li>Working with a small, diverse, and time-constrained group of individuals at a startup in a remote setting</li>
                         </ul>
                     </div>
                     <div>
                         <h3>Advanced Repair Agent</h3>
                         <p>Geek Squad | Mar 2022 - Present</p>
                         <ul>
-                            <li>Earned a Letter of Recommendation from Upper Management for designing and helping to implement a plan to revamp our locations day to day operations, increasing overall productivity by 150% and customer satisfaction </li>
-                            <li>Executing tens of thousands of Software and Hardware fixes on desktops, laptops, tablets, and cell phones</li>
-                            <li>Receiving an award for Top Performing ARA at our location every quarter and was in the top 3% in our District and Region</li>
+                            <li>Earned a Letter of Recommendation from Upper Management for designing and helping to implement a plan to revamp our locations day to day operations, increasing overall productivity by 150% and customer satisfaction.</li>
+                            <li>Executing tens of thousands of Software and Hardware fixes on desktops, laptops, tablets, and cell phones that are using a variety of operating systems.</li>
+                            <li>Receiving an award for Top Performing Advanced Repair Agent at our location and I am consistently in the top 3% of all of the Advanced Repair Agents in our District and Region.</li>
                         </ul>
                     </div>
                 </AnimatedSection>
@@ -147,8 +142,8 @@ const Resume = () => {
                     <div>
                         <h3>Chat RAG: Advanced Coding Assistant</h3>
                         <ul>
-                            <li>Designed an intuitive UI with provider-specific controls, allowing users to fine-tune model parameters such as temperature, max tokens, and context window size, facilitating easy switching between different AI providers.</li>
-                            <li>Developed a sophisticated, chat-based coding assistant leveraging Retrieval-Augmented Generation (RAG) technology, integrating multiple language model providers (Ollama, HuggingFace, NVIDIA NIMs, OpenAI, and Anthropic) with a user-friendly Gradio interface.</li>
+                            <li>Developed a versatile RAG (Retrieval-Augmented Generation) chatbot with a Gradio user-interface, supporting multiple local models from Ollama and Hugging Face and cloud-based from OpenAI, Anthropic, and NVIDIA Nims</li>
+                            <li>Developed a sophisticated, chat-based coding assistant leveraging a Retrieval-Augmented Generation (RAG) pipeline, integrating multiple language model providers (Ollama, HuggingFace, NVIDIA NIMs, OpenAI, and Anthropic) with a user-friendly Gradio interface.</li>
                             <li>Engineered a flexible system featuring dynamic model switching, custom embedding integration (using Hugging Face embeddings), and efficient memory management, demonstrating proficiency in PyTorch, Llama Index, and state-of-the-art language models.</li>
                             <li>Integrated advanced NLP techniques such as custom prompts, chat memory management, and context-aware responses to improve the quality and relevance of the assistant's outputs.</li>
                             <li>Implemented advanced features including local document parsing, GitHub repository integration, and support for multiple vector stores (Neo4j, ChromaDB, Milvus) to enhance the assistant's knowledge base and contextual understanding.</li>
@@ -173,7 +168,7 @@ const Resume = () => {
                         <h3>My Campus Hubs: AI Assistant</h3>
                         <ul>
                             <li>Developed a sophisticated, chat-based campus assistant leveraging Retrieval-Augmented Generation (RAG) technology to retrieve the most recent information about the university</li>
-                            <li>Engineered a 13 million line structured dataset from over 40 million lines of webscrapped unstructured html data from a universities website</li>
+                            <li>Engineered a 13 million line structured dataset from over 200 million lines of webscrapped unstructured HTML and PDF data from a universities website</li>
                             <li>Tested 10+ open-source Large Langauge models with the custom dataset to determine which model would give the ultimate user experience</li>
                             <li>Crafted and tested a custom prompt to ensure the model had the best intructions to provide over 15,000 students with the correct and up-to-date information</li>
                             <li>Designed a custom pipeline that pieces together this ultra-complex AI Assistant</li>
