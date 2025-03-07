d52dcdd8308986fc67b5c8289a3c4bce5a914620
JakeFurtaw <jfurtaw1@students.towson.edu>
2025-03-07 08:57:58 -0500

    updated date

--- jf-coded/src/pages/Projects.jsx
+++ jf-coded/src/pages/Projects.jsx
@@ -247,7 +247,7 @@ const Projects = () => {
                     <Project key={index} {...project} />
                 ))}
             </div>
-            <h4>Last Updated 2/9/25 check my GitHub for my latest project updates. More projects coming soon!</h4>
+            <h4>Last Updated 3/7/25 check my GitHub for my latest project updates. More projects coming soon!</h4>
             <h4>Link Below!</h4>
             <SocialLinks animationType="pulse" />
         </div>
