92622b70a34f936331a201d489bc5f13d181068f
JakeFurtaw <jfurtaw1@students.towson.edu>
2025-03-19 11:24:47 -0400

    edited resume page a bit

--- jf-coded/src/pages/Resume.jsx
+++ jf-coded/src/pages/Resume.jsx
@@ -123,22 +123,29 @@ const Resume = () => {
                     <h2>Work Experience</h2>
                     <div>
                         <h3>Machine Learning Research Engineer</h3>
-                        <p>SurgePoint Software | Aug 2023 - Oct 2024</p>
+                        <p>SurgePoint Software | Hybrid | August 2023 - October 2024</p>
                         <ul>
-                            <li>Developing an innovative AI Assistant using open-source LLMs like Qwen 2.5 7B, Mistral 7B, Mistral-NeMo 12B, and Llama 3.1 8B</li>
-                            <li>Utilizing self-taught data engineering and data preprocessing skills to craft a 13-million-line structured dataset from 200 million lines of unstructured web-scraped HTML and PDF data</li>
-                            <li>Implementing a sophisticated Retrieval-Augmented Generation pipeline to provide the most up-to-date university information to the model to increase semantic relevance scores of retrieved context and reduce model hallucinations</li>
-                            <li>Leveraging self-taught prompt engineering skills to ensure each tested model has a custom well-structured prompt and enough context to answer any query a user might input</li>
-                            <li>Working with a small, diverse, and time-constrained group of individuals at a startup in a mostly remote setting</li>
+                            <li>Collaborated with a small, diverse startup team of engineers doing weekly standups and sprint reviews</li>
+                            <li>Developing an innovative AI Assistant (Chatbot) using open-source LLMs like Qwen 2.5 7B, Mistral 7B, Mistral-NeMo 12B, and Llama 3.1 8B</li>
+                            <li>Utilizing self-taught data engineering and data preprocessing skills to craft a 13-million-line structured dataset from 200 million 
+                                lines of unstructured web-scraped HTML and PDF data increasing semantic relevance scores by 50-75% depending on the query</li>
+                            <li>Designed, engineered, and tested an advanced Retrieval-Augmented Generation pipeline that feeds LLMs my
+                                custom dataset, using Milvus or ChromaDB, created from the university website to reduce model hallucinations</li>
+                            <li>Leveraging self-taught prompt engineering skills to ensure each tested model has a custom well-structured prompt and 
+                                enough context to answer any query a user might input</li>
+                            
                         </ul>
                     </div>
                     <div>
                         <h3>Advanced Repair Agent</h3>
                         <p>Geek Squad | Mar 2022 - Present</p>
                         <ul>
-                            <li>Earned a Letter of Recommendation from Upper Management for designing and helping to implement a plan to revamp our locations day to day operations, increasing overall productivity by 50% resulting in improved customer satisfaction.</li>
-                            <li>Executing tens of thousands of Software and Hardware fixes on desktops, laptops, tablets, and cell phones that are using a variety of operating systems.</li>
-                            <li>Receiving an award for Top Performing Advanced Repair Agent at our location and I am consistently in the top 3% of all of the Advanced Repair Agents in our District and Region.</li>
+                            <li>Earned a Letter of Recommendation from Upper Management for designing and helping to implement a plan to revamp our 
+                                locations day to day operations, increasing overall productivity by 50% resulting in improved customer satisfaction.</li>
+                            <li>Executing tens of thousands of Software and Hardware fixes on desktops, laptops, tablets, and cell phones that are using a variety of 
+                                operating systems.</li>
+                            <li>Receiving an award for Top Performing Advanced Repair Agent at our location and consistently in the top 3% of all of the Advanced Repair Agents 
+                                in our Micro Market and District.</li>
                         </ul>
                     </div>
                 </AnimatedSection>
@@ -148,22 +155,29 @@ const Resume = () => {
                     <div>
                         <h3>Chat RAG: Advanced AI Coding Assistant</h3>
                         <ul>
-                            <li>Developed a versatile RAG (Retrieval-Augmented Generation) coding assitant chatbot with a intricate yet simple Gradio user-interface, supporting multiple local models from Ollama and Hugging Face and cloud-based models from OpenAI, Anthropic, and NVIDIA Nims</li>
-                            <li>Engineered a flexible system featuring dynamic model switching, custom prompt integration, model parameter customization, and efficient chat memory management</li>
-                            <li>Implemented advanced features including local document parsing and injection, GitHub repository integration, and support for multiple vector stores (Neo4j, ChromaDB, Milvus) to enhance the assistant's knowledge base and contextual understanding.</li>
-                            <li>Developed a modular architecture with separate utility classes for model management, parameter updates, and chat functionality, enhancing code maintainability and scalability.</li>
-                            <li>Incorporated GPU memory optimization techniques, including dynamic CUDA cache clearing and garbage collection, to improve performance and resource utilization across different model providers.</li>
-                            <li>Implemented robust error handling, informative user notifications, and dynamic UI updates to enhance the application's reliability and user experience.</li>
-                            <li>Demonstrated strong skills in ML engineering, AI application design, and the integration of multiple AI services and data sources into a cohesive, user-centric application.</li>
+                            <li>Developed a versatile RAG (Retrieval-Augmented Generation) coding assitant chatbot with a intricate yet simple Gradio user-interface, 
+                                supporting multiple local models from Ollama and Hugging Face and cloud-based models from OpenAI, Anthropic, and NVIDIA Nims</li>
+                            <li>Engineered a flexible system featuring dynamic model switching, custom prompt integration, model parameter customization, and efficient 
+                                chat memory management</li>
+                            <li>Designed flexible data ingestion from three diverse sources including local document parsing and injection, GitHub repository integration, 
+                                and support for multiple vector stores (Neo4j, ChromaDB, Milvus) to enhance the assistant's knowledge base and contextual understanding.</li>
+                            <li>Developed a modular architecture with separate utility classes for model management, parameter updates, and chat functionality, enhancing 
+                                code maintainability and scalability.</li>
+                            <li>Incorporated GPU memory optimization techniques, including dynamic CUDA cache clearing and garbage collection, to improve performance 
+                                and resource utilization across different model providers.</li>
                         </ul>
                     </div>
                     <div>
                         <h3>Using an ASTNN to Automatically Identify Equivalent Mutants</h3>
                         <ul>
-                            <li>Participated in Scrum practices in an agile development environment, participating in sprint planning, daily standups, and reviews</li>
-                            <li>Enhanced and modernized the ASTNN codebase by upgrading to Python 3.10, updating library dependencies, implementing model persistence and inference capabilities, and integrating learning rate as a hyperparameter in the ASTNN estimator</li>
-                            <li>Expanded data processing and mutation capabilities through integrating comprehensive mutant dataset from MutantBench, developing custom parser for mutation insertion in original programs, and extending pipeline to support Java mutants using javalang</li>
-                            <li>Innovated equivalent mutant identification techniques by creating a novel approach using Code2Vec's code vectors with neural networks and implementing advanced models including sequential and transformer models with Code2Vec embeddings, and a CodeBERT model leveraging NLP techniques</li>
+                            <li>Excelled as a member of a diverse five-man Scrum Team, engaged in sprint planning, daily standups, and sprint reviews</li>
+                            <li>Enhanced and modernized the ASTNN codebase by upgrading to Python 3.10, updating library dependencies, implementing model persistence 
+                                and inference capabilities, and integrating learning rate as a hyperparameter in the ASTNN estimator</li>
+                            <li>Expanded data processing and mutation capabilities through integrating comprehensive mutant dataset from MutantBench, developing 
+                                custom parser for mutation insertion in original programs, and extending pipeline to support Java mutants using javalang</li>
+                            <li>Innovated equivalent mutant identification techniques by creating a novel approach using Code2Vec's code vectors with neural networks 
+                                and implementing advanced models including sequential and transformer models with Code2Vec embeddings, and a CodeBERT model 
+                                leveraging NLP techniques</li>
                             <li>Applied machine learning best practices by addressing class imbalance through undersampling and oversampling techniques</li>
                             <li>Demonstrated proficiency in PyTorch, Scikit-Learn, TensorFlow, and various NLP tools</li>
                         </ul>
@@ -171,27 +185,34 @@ const Resume = () => {
                     <div>
                         <h3>My Campus Hubs: AI Assistant</h3>
                         <ul>
-                            <li>Developed a sophisticated, chat-based campus assistant leveraging Retrieval-Augmented Generation (RAG) technology to retrieve the most recent information about the university</li>
-                            <li>Engineered a 13 million line structured dataset from over 200 million lines of webscrapped unstructured HTML and PDF data from a universities website</li>
+                            <li>Engineered a 13 million line structured dataset from over 200 million lines of webscrapped unstructured HTML and PDF data from a 
+                                universities website</li>
+                            <li>Developed a sophisticated, chat-based campus assistant leveraging Retrieval-Augmented Generation (RAG) technology to retrieve the 
+                                most recent information about the university from my dataset</li>
                             <li>Tested 10+ open-source Large Langauge models with the custom dataset to determine which model would give the ultimate user experience</li>
-                            <li>Crafted and tested a custom prompt to ensure the model had the best intructions to provide over 15,000 students with the correct and up-to-date information</li>
-                            <li>Designed a custom pipeline that pieces together this ultra-complex AI Assistant</li>
+                            <li>Crafted and tested custom prompts to ensure the model had the best intructions to provide over 15,000 students with the correct and 
+                                up-to-date information</li>
                         </ul>
                     </div>
                     <div>
                         <h3>HealthG: Personalized Health and Fitness AI Assistant</h3>
                         <ul>
-                            <li>Developed a sophisticated, chat-based health assistant using Retrieval-Augmented Generation (RAG) technology, integrating NVIDIA and Mistrals Mistral-Nemo language model with a Gradio interface for personalized health and fitness advice</li>
-                            <li>Implemented advanced features including persistent chat storage, long-term memory, and user-specific chat history for enhanced contextual understanding and personalization</li>
+                            <li>Developed a sophisticated, chat-based health assistant using Retrieval-Augmented Generation (RAG) technology, integrating 
+                                NVIDIA and Mistrals Mistral-Nemo language model with a Gradio interface for personalized health and fitness advice</li>
+                            <li>Implemented advanced features including persistent chat storage, long-term memory, and user-specific chat history for enhanced 
+                                contextual understanding and personalization</li>
                             <li>Designed an intuitive UI with user authentication, allowing personalized chat experiences and easy access to past conversations</li>
-                            <li>Developed a modular architecture with separate utility classes, incorporated GPU optimization techniques, and implemented robust chat functionality with streaming responses</li>
+                            <li>Developed a modular architecture with separate utility classes, incorporated GPU optimization techniques, and implemented robust 
+                                chat functionality with streaming responses</li>
                         </ul>
                     </div>
                     <div>
                         <h3>Image Alter</h3>
                         <ul>
-                            <li>Developed a Gradio-based web application for image generation and editing using state-of-the-art Stable Diffusion models, implementing both text-to-image and image-to-image functionalities</li>
-                            <li>Designed an intuitive user interface with customizable parameters (image dimensions, inference steps, guidance scale, seed selection) and features for generating multiple images simultaneously</li>
+                            <li>Developed a Gradio-based web application for image generation and editing using state-of-the-art Stable Diffusion models, 
+                                implementing both text-to-image and image-to-image functionalities</li>
+                            <li>Designed an intuitive user interface with customizable parameters (image dimensions, inference steps, guidance scale, seed selection) 
+                                and features for generating multiple images simultaneously</li>
                             <li>Optimized model loading and inference using PyTorch and CUDA for efficient GPU utilization</li>
                             <li>Utilized the Hugging Face Diffusers library to leverage pre-trained models and pipelines effectively</li>
                         </ul>
@@ -205,11 +226,14 @@ const Resume = () => {
                     <img style={{width: 200, height: 200}} src={TULogo} alt="Towson Logo" />
                         <h3>Bachelor of Science in Computer Science, Software Engineering Concentration</h3>
                         <p>Towson University, Towson, MD | Jan 2022 - Dec 2023</p>
-                        <p><strong>Coursework:</strong> Artificial Intelligence, Database Management Systems, Software Quality Testing & Assurance, Data Communications and Networking, Data Structures and Algorithms, Requirement Analysis and Modeling, Programming Languages: Design and Implementation, Operating Systems, Software Project Practicum,  Software Engineering, Web Based Programming</p>
+                        <p><strong>Coursework:</strong> Artificial Intelligence, Database Management Systems, Software Quality Testing & Assurance, 
+                        Data Communications and Networking, Data Structures and Algorithms, Requirement Analysis and Modeling, 
+                        Programming Languages: Design and Implementation, Operating Systems, Software Project Practicum,  
+                        Software Engineering, Web Based Programming</p>
                     </div>
                     <div>
                     <img style={{width: 200, height: 200}}src={HCCLogo} alt="HCC Logo" />
-                        <h3>Associate of Science in Computer Science</h3>
+                        <h3>Associate of Arts in Computer Science</h3>
                         <p>Howard Community College, Columbia, MD | Aug 2019 - Dec 2021</p>
                         <p><strong>Coursework:</strong> Computer Science I: Intro to C++, Computer Sceince II: Advanced C++ , Intro to Python</p>
                     </div>
